8a2c1f6e8d263439dff8305e03d9dc6b8654b6a4
DanielaChavez <daniela.chavez@est.ikiam.edu.ec>
2022-08-05 19:16:52 -0500

    Modificaciones Examen Parcial 2

--- miningscience.py
+++ miningscience.py
@@ -23,9 +23,9 @@ def download_pubmed (keyword):
                            retmode="text", 
                            retstart=0,
                            retmax=543, webenv=webenv, query_key=query_key)
-    data = handle.read()
-    dataexp = re.sub(r'\n\s{6}','', data)
-    return dataexp
+    data_pudmed = handle.read()
+    data = re.sub(r'\n\s{6}','', data_pudmed)
+    return data
     
 # Función 2
 def science_plots (archivo):
@@ -33,23 +33,23 @@ def science_plots (archivo):
     Permite generar un diagrama de pastel de los países a los que pertenecen los autores que han realizado publicaciones sobre un tema en particular
     """
     
-    elimcorreos = re.sub(r'\s[\w._%+-]+@[\w.-]+\.[a-zA-Z]{1,4}','',archivo)
-    elimpuntos = re.sub(r'\..\d.\,',',',elimcorreos)
-    elimnumb = re.sub(r'\..\d.','',elimpuntos)
-    x=elimnumb[1:].split('PMID-')
+    correos = re.sub(r'\s[\w._%+-]+@[\w.-]+\.[a-zA-Z]{1,4}','',archivo)
+    puntos = re.sub(r'\..\d.\,',',',correos)
+    numb = re.sub(r'\..\d.','',puntos)
+    x=numb[1:].split('PMID-')
     
-    CountriesA=[]
+    Countries_A=[]
     for PMID in x:
         q=PMID.split('\n')
         for fila in q:
             w=fila.split(' ')
             if w[0] == 'AD':
                 e=fila.split(',')
-                CountriesA.append(e[-1])
+                Countries_A.append(e[-1])
     
     a=0
-    CountriesB =[0]*len(CountriesA)
-    for lis in CountriesA:
+    Countries_B =[0]*len(Countries_A)
+    for lis in Countries_A:
         bytes(lis,encoding="utf8")
         if lis != '':
             w=lis
@@ -59,36 +59,36 @@ def science_plots (archivo):
                 w = re.sub (r'\.$','',w)
             w = re.sub (r'\.$','',w)
             w = re.sub (r'\s$','',w)
-        CountriesB[a]=w  #Saca las ultimas palabras
+        Countries_B[a]=w
         a=a+1
         
-    Contries_all = ['Andorra','United Arab Emirates ','Afghanistan','Antigua and Barbuda','Anguilla','Albania','Armenia','Netherlands Antilles','Angola','Antarctica','Argentina','American Samoa','Austria','Australia','Aruba','Azerbaijan','Bosnia and Herzegovina','Barbados','Bangladesh','Belgium','Burkina Faso','Bulgaria','Bahrain', 'Burundi','Benin','Bermuda','Brunei','Bolivia', 'Brazil','Bahamas','Bhutan','Bouvet Island','Botswana','Belarus','Belize','Canada','Cocos [Keeling] Islands','Congo [DRC]','Central African Republic','Congo Republic', 'Switzerland',"Côte d'Ivoire",'Cook Islands','Chile','Cameroon','China','Colombia','Costa Rica','Cuba', 'Cape Verde','Christmas Island','Cyprus','Czech Republic','Germany','Djibouti','Denmark','Dominica','Dominican Republic','Algeria','Ecuador' ,'Estonia','Egypt','Western Sahara','Eritrea','Spain','Ethiopia','Finland','Fiji','Falkland Islands [Islas Malvinas]','Micronesia','Faroe Islands','France','Gabon', 'United Kingdom','Grenada','Georgia','French Guiana','Guernsey','Ghana','Gibraltar','Greenland','Gambia', 'Guinea','Guadeloupe','Equatorial Guinea','Greece','South Georgia and the South Sandwich Islands','Guatemala','Guam','Guinea-Bissau','Guyana','Gaza Strip','Hong Kong','Heard Island and McDonald Islands','Honduras','Croatia', 'Haiti','Hungary','Indonesia','Ireland' ,'Israel','Isle of Man','India','British Indian Ocean Territory','Iraq', 'Iran','Iceland','Italy','Jersey','Jamaica','Jordan', 'Japan','Kenya','Kyrgyzstan','Cambodia','Kiribati','Comoros','Saint Kitts and Nevis','North Korea','South Korea','Kuwait','Cayman Islands','Kazakhstan','Laos','Lebanon','Saint Lucia','Liechtenstein','Sri Lanka','Liberia','Lesotho','Lithuania','Luxembourg','Latvia' ,'Libya','Morocco','Monaco','Moldova','Montenegro','Madagascar','Marshall Islands','Macedonia [FYROM]','Mali','Myanmar [Burma]','Mongolia' ,'Macau','Northern Mariana Islands','Martinique','Mauritania','Montserrat','Malta','Mauritius','Maldives','Malawi','Mexico','Malaysia' ,'Mozambique','Namibia','New Caledonia','Niger','Norfolk Island','Nigeria','Nicaragua','The Netherlands','Norway','Nepal','Nauru', 'Niue','New Zealand','Oman','Panama','Peru','French Polynesia', 'Papua New Guinea','Philippines','Pakistan','Poland','Saint Pierre and Miquelon' ,'Pitcairn Islands','Puerto Rico','Palestinian Territories','Portugal','Palau','Paraguay','Qatar','Réunion','Romania', 'Serbia','Russia' ,'Rwanda','Saudi Arabia','Solomon Islands','Seychelles','Sudan','Sweden','Singapore','Saint Helena','Slovenia', 'Svalbard and Jan Mayen','Slovakia','Sierra Leone','San Marino','Senegal','Somalia','Suriname','São Tomé and Príncipe','El Salvador','Syria', 'Swaziland' ,'Turks and Caicos Islands','Chad','French Southern Territories','Togo','Thailand','Tajikistan','Tokelau','Timor-Leste','Turkmenistan' ,'Tunisia','Tonga','Turkey','Trinidad and Tobago','Tuvalu','Taiwan','Tanzania','Ukraine','Uganda','U.S. Minor Outlying Islands','United States of America','Uruguay','Uzbekistan','Vatican City','Saint Vincent and the Grenadines','Venezuela', 'British Virgin Islands','U.S. Virgin Islands','Vietnam','Vanuatu','Wallis and Futuna','Samoa','Kosovo','Yemen','Mayotte','South Africa','Zambia','Zimbabwe']
-    CountriesC=CountriesB
+    Contries_all=['Andorra','United Arab Emirates ','Afghanistan','Antigua and Barbuda','Anguilla','Albania','Armenia','Netherlands Antilles','Angola','Antarctica','Argentina','American Samoa','Austria','Australia','Aruba','Azerbaijan','Bosnia and Herzegovina','Barbados','Bangladesh','Belgium','Burkina Faso','Bulgaria','Bahrain', 'Burundi','Benin','Bermuda','Brunei','Bolivia', 'Brazil','Bahamas','Bhutan','Bouvet Island','Botswana','Belarus','Belize','Canada','Cocos [Keeling] Islands','Congo [DRC]','Central African Republic','Congo Republic', 'Switzerland',"Côte d'Ivoire",'Cook Islands','Chile','Cameroon','China','Colombia','Costa Rica','Cuba', 'Cape Verde','Christmas Island','Cyprus','Czech Republic','Germany','Djibouti','Denmark','Dominica','Dominican Republic','Algeria','Ecuador' ,'Estonia','Egypt','Western Sahara','Eritrea','Spain','Ethiopia','Finland','Fiji','Falkland Islands [Islas Malvinas]','Micronesia','Faroe Islands','France','Gabon', 'United Kingdom','Grenada','Georgia','French Guiana','Guernsey','Ghana','Gibraltar','Greenland','Gambia', 'Guinea','Guadeloupe','Equatorial Guinea','Greece','South Georgia and the South Sandwich Islands','Guatemala','Guam','Guinea-Bissau','Guyana','Gaza Strip','Hong Kong','Heard Island and McDonald Islands','Honduras','Croatia', 'Haiti','Hungary','Indonesia','Ireland' ,'Israel','Isle of Man','India','British Indian Ocean Territory','Iraq', 'Iran','Iceland','Italy','Jersey','Jamaica','Jordan', 'Japan','Kenya','Kyrgyzstan','Cambodia','Kiribati','Comoros','Saint Kitts and Nevis','North Korea','South Korea','Kuwait','Cayman Islands','Kazakhstan','Laos','Lebanon','Saint Lucia','Liechtenstein','Sri Lanka','Liberia','Lesotho','Lithuania','Luxembourg','Latvia' ,'Libya','Morocco','Monaco','Moldova','Montenegro','Madagascar','Marshall Islands','Macedonia [FYROM]','Mali','Myanmar [Burma]','Mongolia' ,'Macau','Northern Mariana Islands','Martinique','Mauritania','Montserrat','Malta','Mauritius','Maldives','Malawi','Mexico','Malaysia' ,'Mozambique','Namibia','New Caledonia','Niger','Norfolk Island','Nigeria','Nicaragua','The Netherlands','Norway','Nepal','Nauru', 'Niue','New Zealand','Oman','Panama','Peru','French Polynesia', 'Papua New Guinea','Philippines','Pakistan','Poland','Saint Pierre and Miquelon' ,'Pitcairn Islands','Puerto Rico','Palestinian Territories','Portugal','Palau','Paraguay','Qatar','Réunion','Romania', 'Serbia','Russia' ,'Rwanda','Saudi Arabia','Solomon Islands','Seychelles','Sudan','Sweden','Singapore','Saint Helena','Slovenia', 'Svalbard and Jan Mayen','Slovakia','Sierra Leone','San Marino','Senegal','Somalia','Suriname','São Tomé and Príncipe','El Salvador','Syria', 'Swaziland' ,'Turks and Caicos Islands','Chad','French Southern Territories','Togo','Thailand','Tajikistan','Tokelau','Timor-Leste','Turkmenistan' ,'Tunisia','Tonga','Turkey','Trinidad and Tobago','Tuvalu','Taiwan','Tanzania','Ukraine','Uganda','U.S. Minor Outlying Islands','United States of America','Uruguay','Uzbekistan','Vatican City','Saint Vincent and the Grenadines','Venezuela', 'British Virgin Islands','U.S. Virgin Islands','Vietnam','Vanuatu','Wallis and Futuna','Samoa','Kosovo','Yemen','Mayotte','South Africa','Zambia','Zimbabwe']
+    Countries_C=Countries_B
     h=Contries_all
     f=len(h)
     CountriesCount=[0]*f
     k=0
     for elem in h:
         d=0
-        for comp in CountriesC:
+        for comp in Countries_C:
             if elem == str(comp):
                 d=d+1
-        CountriesCount[k]=d  #Contador
+        CountriesCount[k]=d
         k=k+1
     
-    CountriesD=[]
+    Countries_D=[]
     Counter=[]
     o=0
     for line in CountriesCount:
-        if str(line) != '0': #Tomo los paises que tinen un valor
-            Counter.append(line) #Nùmero de autores
+        if str(line) != '0': 
+            Counter.append(line) 
             m=Contries_all[o]
-            CountriesD.append(m) # Los paìses
+            Countries_D.append(m) 
         o=o+1
 
-    TableA = pd.DataFrame({'Country' : CountriesD,
+    Table_A = pd.DataFrame({'Country' : Countries_D,
                            'num_auth' : Counter})
-    Order=TableA.sort_values(by=['num_auth'], ascending=[False])
+    Order=Table_A.sort_values(by=['num_auth'], ascending=[False])
     Taken = Order.iloc[0:5]
     suma=Taken['num_auth'].sum()
     iu= Taken.iloc[:,0]
@@ -101,7 +101,7 @@ def science_plots (archivo):
     for number in s:
         xa=(number/suma)*100
         prom.append(xa)
-    TableB = pd.DataFrame({'Country' : su,
+    Table_B = pd.DataFrame({'Country' : su,
                            'Porcent' : prom})
     
     fig1, ax1 = plt.subplots()
@@ -109,6 +109,6 @@ def science_plots (archivo):
             shadow=True, startangle=90)
     ax1.axis('equal')  
     plt.show()
-    plt.savefig('img/Gráficadepie.....jpg', dpi=300)
+    plt.savefig('img/Gráficadepie.....jpg', dpi=500)
 
-    return (TableB)
+    return (Table_B)
